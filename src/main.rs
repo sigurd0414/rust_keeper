806c2e6c2f23293995af469ea7e2d17824f11a50
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 07:01:48 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,9 @@ struct Person;
 struct Name(String);
 
 fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(hello_world_system.system())
-        .add_startup_system(add_people.system())
-        .add_system(greet_people.system())
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hello_world)
         .run();
 }
 
