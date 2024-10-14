7732c9d1dd5350ecdd83ddada2b9a2e1c9ab6895
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 06:51:13 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use bevy::prelude::*;
-use core_logic::player::PlayerBehaviorPlugin;
 
 fn main() {
-    App::new() // 使用 App::new() 而不是 App::build()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(PlayerBehaviorPlugin)
-        .run();
+    App::new().run();
 }
+The use bevy::prel
