53a12ad794820683d22ea33d7e9ae92f83a8009c
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 07:08:16 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Person;
 #[derive(Component)]
-struct Name (String);
+struct Name(String);
 
 fn main() {
     App::new()
         .add_systems(Startup, add_people)
-        .add_systems(Update, hello_world)
+        .add_systems(Update, (hello_world, greet_people))
         .run();
 }
 
@@ -20,4 +20,10 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
