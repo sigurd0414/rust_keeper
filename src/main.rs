52b3c6e1eaafb2232bbe6e70556152c9f80574f9
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 07:09:03 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Name(String);
 fn main() {
     App::new()
         .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
         .run();
 }
 
@@ -25,5 +25,14 @@ fn add_people(mut commands: Commands) {
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in &query {
         println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don't need to change any other names.
+        }
     }
 }
