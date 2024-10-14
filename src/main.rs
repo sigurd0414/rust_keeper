d1bfb1a52280bf7149756456149c9df24ac20c33
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 07:03:02 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,3 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
-    }
-}
