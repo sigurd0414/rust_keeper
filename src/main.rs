014cb03a6a50fc6416df0de1e5a63db580183015
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 06:55:54 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hello_world)
+        .run();
 }
