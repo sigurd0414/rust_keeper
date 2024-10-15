fac9ff1e86de0a189edc8fbe0dafc0a92d4889fd
sigurd0414 <sigurd@cco.com.tw>
2024-10-15 08:01:34 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,33 @@
+// src/main.rs
+
 use bevy::prelude::*;
 
 #[derive(Component)]
 struct Person;
 #[derive(Component)]
-struct Name(String);
+struct Name(pub String);
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(hello_world_system)
+        .add_startup_system(add_people)
+        .add_system(greet_people)
         .run();
 }
 
-fn hello_world() {
+fn hello_world_system() {
     println!("Hello, world!");
 }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person, Name("Alice".to_string())));
+    commands.spawn((Person, Name("Bob".to_string())));
+    commands.spawn((Person, Name("Charlie".to_string())));
 }
 
 fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
     }
 }
